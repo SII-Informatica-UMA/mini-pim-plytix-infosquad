9e9b06c0d827b433b78a98429c2c8fdfda70e38e
AdamAhmedMohamed <mohamedahmedadam08@gmail.com>
2025-05-04 13:32:15 +0200

    Eliminando archivos innecesarios

--- backend/.mvn/src/main/java/minipimplytixinfosquad/entidades/repositories/CuentaRepository.java
+++
@@ -1,8 +0,0 @@
-package minipimplytixinfosquad.entidades.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import minipimplytixinfosquad.entidades.entities.Cuenta;
-
-public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
-    // Aquí puedes añadir consultas personalizadas si las necesitas más adelante
-}
